20e64164721c65867497015219d8b0683b048191
John Bourgeois <>
2021-01-26 10:09:41 -0800

    Evans Help

--- TinyDesk.py
+++ TinyDesk.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 import re
 import csv
 
-outfile = open('tables.csv', "w", newline='')
+outfile = open('tables.csv', "w", encoding = "utf-8", newline='')
 writer = csv.writer(outfile)
 
 
@@ -18,35 +18,24 @@ html = html_bytes.decode("utf-8")
 
 soup = BeautifulSoup(html, "lxml")
 
-#tables = soup.find_all('table')
-#print(tables[0])
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 list_of_tables = []
 for tables in soup.find_all("table"):
 
     list_of_rows = []
+
     for row in tables.find_all('tr'):
+
         list_of_cells = []
-        for cell in row.find_all(["th","td"]):
-            text = cell.text
-            list_of_cells.append(text)
+
+        cells = row.find_all("td")
+
+        if len(cells)==2  and (cells[0].text.split(' ')[0] in months):
+            print(cells[0].text)
+            list_of_cells.append(cells[1].text)
+
 
         list_of_rows.append(list_of_cells)
 
     for item in list_of_rows:
         writer.writerow(item)
-        #print(' '.join(item))
-
-#title_index = html.find("<title>")
-#start_index = title_index + len("<title>")
-#end_index = html.find("</title>")
-#title = html[start_index:end_index]
-#print(title)
-
-
-#pattern = "<title.*?>.*?</title.*?>"
-#match_results = re.search(pattern, html, re.IGNORECASE)
-#new_title = match_results.group()
-#new_title = re.sub("<.*?>", "", title)
-
-#print(new_title)
-#print(soup.get_text())
